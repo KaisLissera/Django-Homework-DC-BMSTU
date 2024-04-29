ce9d94b0be2e31c6d1bb81debfec947b5b30d2cd
Kais Lissera <ka.pohne2014@gmail.com>
2024-04-29 20:35:24 +0300

    Lecture part

--- project_tracker/quality_control/urls.py
+++ project_tracker/quality_control/urls.py
@@ -5,8 +5,8 @@ app_name = 'quality_control'
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('bugs', views.bug_list, name = 'bug_list'),
-    path('bugs/<int:bug_id>', views.bug_detail),
-    path('features', views.feature_list, name = 'feature_list'),
-    path('features/<int:feature_id>', views.feature_id_detail),
+    path('bugs/', views.bug_list, name = 'bug_list'),
+    path('bugs/<int:bug_id>/', views.bug_detail),
+    path('features/', views.feature_list, name = 'feature_list'),
+    path('features/<int:feature_id>/', views.feature_id_detail),
 ]
